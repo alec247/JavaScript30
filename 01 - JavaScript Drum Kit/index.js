95ab242dad8edcfc1b613dafd54771fcbadf7a27
alec247 <alec247@gmail.com>
2023-02-08 13:50:22 -0800

    Add function to remove css transition

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,14 +1,28 @@
-window.addEventListener("keydown", (event) => {
+function playOnKeyPress(event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
     if (!audio) return; // stop function if wrong key pressed
     audio.currentTime = 0; // rewind to beginning if already playing
     audio.play();
+    key.classList.add("playing");
+}
 
-    // add and remove playing class to key pressed
-    key.classList.toggle("playing");
-    setTimeout(() => {
-        key.classList.toggle("playing");
-    }, 150);
+function playOnClick(event) {
+    console.log(event.target);
+    
+}
+
+function removeTransition(event) {
+    if (event.propertyName !== "transform") return; // skip if not transform (otherwise will fire numerous times)
+
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => {
+    key.addEventListener("click", playOnClick);
+    key.addEventListener("transitionend", removeTransition)
 });
+
+window.addEventListener("keydown", playOnKeyPress);
